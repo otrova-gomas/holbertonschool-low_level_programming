34759b83067a0f51c6f3b9a800872c50bf2e158c
otrova-gomas <javieremiliocastro88@gmail.com>
2023-02-17 18:37:00 +0000

    program that prints

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -8,5 +8,5 @@
 int main(void)
 {
   
-  puts("/" Programming is like building a multilingual puzzle"\n"); return (0);
+  puts("\" Programming is like building a multilingual puzzle\n"); return (0);
 }
