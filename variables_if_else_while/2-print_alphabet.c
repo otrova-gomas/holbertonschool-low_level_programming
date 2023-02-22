7d234970ca2824f56a37efca175711f6be3b3793
otrova-gomas <javieremiliocastro88@gmail.com>
2023-02-22 02:45:28 +0000

    added task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet.
+ *
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char AA
+
+	for (AA = 'a'; AA <='z' ; AA++)
+	{
+		putchar(AA);
+	}
+	putchar('\n');
+	return (0);
+}
