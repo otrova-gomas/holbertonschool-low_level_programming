620c2851da9c2e2491140443c5856f10b5a19e7e
otrova-gomas <javieremiliocastro88@gmail.com>
2023-02-22 03:28:26 +0000

    added task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Prints the alphabet without q and e.
+ *
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+  Char aa;
+
+ for(aa = 'a'; aa <= 'z'; aa++)
+   if (aa != 'e' && la != 'q')
+   putchar(aa);
+
+
+	putchar('\n');
+	
+	return (0);
+}
