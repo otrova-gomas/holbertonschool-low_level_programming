b9b0afcdff55a2c7640293aa2f19b3512a3554b4
otrova-gomas <javieremiliocastro88@gmail.com>
2023-02-22 06:13:34 +0000

    task 7

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - print all the numbers of base 16 in lowercase
+ *
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+  int number;
+  char letter;
+
+  for (number = '0'; number <= 10; number++)
+    putchar((number % 10) + '0');
+
+  for (letter = 'a'; letter <='f', letter++)
+    putchar(letter);
+	    
+putchar('\n');
+	    
+return (0);
+}
