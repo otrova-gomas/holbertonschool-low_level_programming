1f024f2001b5fd6f5a5d12cab7d4e23b0df5b87b
otrova-gomas <javieremiliocastro88@gmail.com>
2023-02-22 06:42:56 +0000

    fixed 8-print_base16.c:13: ERROR: space required before the open parenthesis '(' 8-print_base16.c:15: ERROR: spaces required around that '<=' (ctx:WxV) 8-print_base16.c:15: ERROR: space required before the open parenthesis '(' 8-print_base16.c:17: ERROR: trailing whitespace

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,10 +10,10 @@ int main(void)
 {
 int number;
 char letter;
-for(number = 0; number < 10; number++)
+for (number = 0; number < 10; number++)
 putchar((number % 10) + '0');
-for(letter = 'a'; letter <='f'; letter++)
+for (letter = 'a'; letter <= 'f'; letter++)
 putchar(letter);
-putchar('\n');    
+putchar('\n');
 return (0);
 }
