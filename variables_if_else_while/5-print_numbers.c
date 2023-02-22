e94f90b79e3371ed107a5833fd73e25746b3d228
otrova-gomas <javieremiliocastro88@gmail.com>
2023-02-22 03:39:48 +0000

    added task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 9.
+ *
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	  putchar((num % 10) + '0');
+       
+	putchar('\n');
+	return (0);
+}
