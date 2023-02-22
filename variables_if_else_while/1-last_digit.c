6317423ec1ce02235845aac7232fcd5207e01997
otrova-gomas <javieremiliocastro88@gmail.com>
2023-02-22 02:32:25 +0000

    fixed typos

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -6,16 +6,15 @@
 int main(void)
 {
   int n;
-  ina t;
+  int t;
 
   srand(time(0));
   n = rand() - RAND_MAX / 2;
   t = n % 10;
-  
+}
   if(t > 5)
     {
       printf("Last digit of %d is %d and is greater than 5\n", n, t);
-}
 	else if (t == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, t);
