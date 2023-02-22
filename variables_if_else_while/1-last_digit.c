efff81a1a3605933ea726d3f08d5d0f9774ff81e
otrova-gomas <javieremiliocastro88@gmail.com>
2023-02-22 02:27:41 +0000

    added task 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,16 +1,28 @@
 /**
  * main - Print the last digit of a randomly generated number
  *
- *
  * Return: Always 0 
  */
 int main(void)
 {
   int n;
+  ina t;
 
   srand(time(0));
   n = rand() - RAND_MAX / 2;
-
-  if((n%10) > 5)
+  t = n % 10;
+  
+  if(t > 5)
     {
-      printf{"
+      printf("Last digit of %d is %d and is greater than 5\n", n, t);
+}
+	else if (t == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, t);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, t);
+	}
+	return (0);
+}
