1e8ad5dfeb4a44063a46107e8cd158e76b3667df
otrova-gomas <javieremiliocastro88@gmail.com>
2023-02-22 06:33:59 +0000

    task #9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combinations of numbers between 0-9
+ *
+ *
+ */
+int main(void)
+{
+  int sdn;
+
+for (sdn = 0; sdn <=9; sdn++)
+{
+putchar((sdn % 10);
+if (sdn==9)
+continue;
+putchar (',')
+putchat(' ')
+}
+putchar ('\n');
+return (0);
+}
