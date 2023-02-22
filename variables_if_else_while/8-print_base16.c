8b80f23919bd7b2055f7c91ac46caed99cb58842
otrova-gomas <javieremiliocastro88@gmail.com>
2023-02-22 06:24:41 +0000

    fixed an issue

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,7 +10,7 @@ int main(void)
 {
 int number;
 char letter;
-for(number = 0; number <= 10; number++)
+for(number = 0; number < 10; number++)
 putchar((number % 10) + '0');
 for(letter = 'a'; letter <='f'; letter++)
 putchar(letter);
