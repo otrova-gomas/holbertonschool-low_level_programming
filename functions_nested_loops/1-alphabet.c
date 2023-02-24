377ae861041068cc8c1275a0a72cd275cd657622
otrova-gomas <javieremiliocastro88@gmail.com>
2023-02-24 00:52:32 +0000

    added function 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - print the alphabet in lowercase
+ *
+ *Always Return: 0
+ *
+ */
+void main(void)
+
+{
+char az;
+
+for(az = 'a'; az <= 'z'; az++)
+{
+putchar (az);
+}
+ 
+putchar ('\n');
+}
