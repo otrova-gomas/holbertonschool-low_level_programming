e5ea9751bce17308276faeae2785011fa401d971
otrova-gomas <javieremiliocastro88@gmail.com>
2023-02-27 21:45:02 +0000

    fixed syntax problems and formated in betty style

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,14 +7,13 @@
  */
 int print_last_digit(int num)
 {
-    int last_digit = num % 10;
-    if (last_digit < 0)
-      {
-        last_digit = (-1 *last_digit);
-      }
-    _putchar('0' + last_digit);
-    _putchar('\n');
+int last_digit = num % 10;
+if (last_digit < 0)
+{
+last_digit = (-1 *last_digit);
+}
+_putchar('0' + last_digit);
 
-    return (last_digit);
+return (last_digit);
 
 }
