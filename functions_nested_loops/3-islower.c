c51cb066e536994c004c9f116579053b0175d405
otrova-gomas <javieremiliocastro88@gmail.com>
2023-02-27 15:57:53 +0000

    added the thrid task

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * int _islower(int c)
+ * desc c is lowercase
+ * func @c lowercase
+ *
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+
+return (1);
+else
+return (0);
+}
