91e75fe3e9b68cdd8043e7946a49e2b5a97813fd
otrova-gomas <javieremiliocastro88@gmail.com>
2023-02-27 21:47:25 +0000

    fixed syntax problems and formated in betty style

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
-*print_last_digit - Print the last digit of a number                                                                                                                                       
- * @num: number to print the last digit of                                                                                                                                                  
- *                                                                              
- * Return: The value of the last digit   
+*print_last_digit - Print the last digit of a number
+ * @num: number to print the last digit of
+ * Return: The value of the last digit
  */
 int print_last_digit(int num)
 {
 int last_digit = num % 10;
 if (last_digit < 0)
 {
-last_digit = (-1 *last_digit);
+last_digit = (-1 * last_digit);
 }
 _putchar('0' + last_digit);
 
