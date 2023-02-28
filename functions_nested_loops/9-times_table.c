fc5c103a01cfbfa1d1410a3bdcf23f98626692b7
otrova-gomas <javieremiliocastro88@gmail.com>
2023-02-28 00:19:22 +0000

    fixed last spaces

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,28 +7,31 @@
  */
 void times_table(void)
 {
-    int num1, num2, result;
-    for (num1 = 0; num1 <= 9; num1++)
-    {
-        for (num2 = 0; num2 <= 9; num2++)
-        {
-            result = num1 * num2;
-            if (result > 9)
+int num1, num2, result;
+for (num1 = 0; num1 <= 9; num1++)
+{
+for (num2 = 0; num2 <= 9; num2++)
+{
+result = num1 * num2;
+if (result > 9)
+{
+_putchar(result / 10 + '0');
+}
+else
+{
+_putchar(' ');
+}
+_putchar(result % 10 + '0');
+
+if (num2 != 9)
             {
-                _putchar(result / 10 + '0');
-            }
-            else
-            {
-                _putchar(' ');
-               
-            }
-            _putchar(result % 10 + '0');
-            if (num2 != 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-        _putchar('\n');
-    }
+_putchar(',');
+_putchar(' ');
+}
+else
+{
+_putchar('\n');
+}
+}
+}
 }
