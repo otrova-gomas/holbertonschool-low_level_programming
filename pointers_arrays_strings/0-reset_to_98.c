a79fdd42c73657d7cc53c9690cee4a45c7bac474
otrova-gomas <javieremiliocastro88@gmail.com>
2023-02-28 21:25:36 +0000

    fix function

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,11 @@
 #include "main.h"
-#include <stdio>
 /**
  *reset_to_98 - takes int updates the val to 98
  *
  *@n variable
  *
  */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
-int a = 98
-  int*a =*n
-  }
+*n=98
+}
