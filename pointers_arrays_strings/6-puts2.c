9735ac5d57f8546b3e114f41ae62140afea7931e
otrova-gomas <javieremiliocastro88@gmail.com>
2023-03-01 03:54:54 +0000

    added task 6 - print every other character

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other chactr
+ * 
+ * @str: Pointer to the string to be printed.
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+int i;
+
+if (str == 0)
+return;
+
+for (i = 0; str[i] != '\0'; i += 2)
+_putchar(str[i]);
+
+_putchar('\n');
+}
